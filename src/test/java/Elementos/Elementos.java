7bc6e7d2173e497e731a93fa0cb9469e2ab3c557
daniela <daniela.bispo1502@gmail.com>
2022-11-25 10:56:06 -0300

    framework banco original

---
+++ src/test/java/Elementos/Elementos.java
@@ -0,0 +1,46 @@
+package Elementos;
+
+import org.openqa.selenium.By;
+
+public class Elementos {
+	private static final By AbrirContaParaSuaEmpresa = null;
+	private By btnAbrirConta = By.cssSelector("body > div.viewport-primary > nav.nav-desktop > div.right > div.nav-item.nav-item-nova-conta > a");
+	private By btnAbrirContaParaVoce = 	By.id("bt1");
+	private By campoNome = By.id("nome");
+    private By campoTelefone = By.id("telefone") ; 
+    private By campoEmail = By.id("email");
+    private By campoCpf = By.id("cpf");
+    private By btnQueroSerCliente = By.id("btnEnviar");
+    // pessoa juridica
+    private  By btnAbrirContaParaSuaEmpresa = By.id("bt2");
+    private By btnAbrirContaPessoaUnica = By.id("bt3");
+    
+    
+	
+    private By getBtnAbrirConta =By.xpath("/html/body/div[2]/nav[2]/div[2]/div[4]/a");
+		
+	private By getBtnAbrirContaParaSuaEmpresa = By.id("bt2") ;
+	
+	private By getBtnPessoaUnica = By.id("bt3");
+	
+    private By getCampoNome = By.id("") 
+		 
+	
+	private By getCampoTelefone() 
+		
+	}
+	private By getCampoEmail() {
+		return campoEmail;
+	}
+	private  By getCampoCpf() {
+		return campoCpf;
+	
+	}
+	
+	
+
+	
+
+	
+
+
---
+++ target/test-classes/Feature/AberturaConta.feature
@@ -0,0 +1,27 @@
+
+@tag
+Feature: Abertura de conta
+  I want to use this template for my feature file
+
+  @tag1
+  Scenario: Abertura de conta pessoa fisica 
+ # Given que o usuario acesse a url "https://www.original.com.br/"
+  #And selecionar o botao abrir conta 
+#  And selecionar o botao abrir conta para voce
+#  When preencher o formulario de abertura de conta 
+#  And selecionar o botao quero ser cliente 
+#  Then sera exibido o texto falta pouco 
+   
+
+@tag2
+Scenario:  Abertura de conta pessoa juridica
+  Given que o usuario acesse a url "https://www.original.com.br/"
+  And selecionar o botao abrir conta 
+  And selecionar o botao abrir conta para sua empresa
+  And selecionar o botao pessoa unica
+  When preencher o formulario de abertura de conta 
+  Then selecionar o botao enviar
+  
+   
+
+   
